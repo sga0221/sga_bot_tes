0aa052dbefa61df1fc1e3ac06a116ffa29413d8a
Kosuke Otake <sga0221@gmail.com>
2015-08-15 00:15:04 +0900

    標準入力からpostできるように変更. exitでpostをキャンセル。timeで現在時間(ctime())をついっと

--- twi/twi_post.py
+++ twi/twi_post.py
@@ -11,5 +11,33 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-#post
-api.update_status(status = time.ctime())    
+#post関数
+def post(post_word):
+    if post_word == "time":
+        api.update_status(status = time.ctime())    
+        return 1
+    elif post_word == "exit":
+        return -1
+    else:
+        api.update_status(status = post_word)
+        return 1
+def main():
+    try:
+        print("input post words")
+        post_word = input()
+        t = post(post_word)
+
+        if t == 1:
+            print("tweet success")
+        elif t == -1:
+            print("exit success")
+        else:
+            print("undefined error.")
+        
+    except KeyboardInterrupt:
+        print("\nexit: KeyboardInterrupt")
+        exit
+
+if __name__ == "__main__":
+    main()
+    
