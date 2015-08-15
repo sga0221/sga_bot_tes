c4198de92187996d2c1f91cb12a42ac3a665b251
Kosuke Otake <sga0221@gmail.com>
2015-08-15 13:19:31 +0900

    stream apiからpublic timelineを取得し、jsonファイルを標準出力へ出すプログラムの実装

---
+++ twi_js/auth_sga.js
@@ -0,0 +1,14 @@
+exports.consumer_key = function (){
+    return "jjyUllEvVvcZnlmpYmrr8MDyb";
+}
+
+exports.consumer_secret = function(){
+    return "4FpW5eHiFDohGOZ3wUY3gOaQcaSFLxVZqNW9RCkBHUtWabNJd0";
+}
+exports.access_token_key = function (){
+    return "3314842159-YlA9I2c8KbUoYQEREYRsu590T9WpYwTcUj6ZAMj";
+}
+
+exports.access_token_secret = function(){
+    return "xTcc2DKH9l093CjxyQwt4vL1RIMFhqkDVFbUMzbxwvKYi";
+}
